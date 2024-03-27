50907900855b7eb6d183eae0fb7ff5cfbd3d8ef9
BOTNIZAM <123469628+BOTNIZAM@users.noreply.github.com>
2024-03-27 17:10:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append()
+ADMINS.append(1269963921)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
